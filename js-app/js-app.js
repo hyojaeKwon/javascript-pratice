89d851e52407066db41fc9648722e460ca347dd9
HyojaeKwon-GIST20 <khj010909@gm.gist.ac.kr>
2021-09-10 21:08:42 +0900

    creating the class name

--- js-app/js-app.js
+++ js-app/js-app.js
@@ -1,9 +1,24 @@
 const loginInput = document.querySelector("#login-section input");
 const loginButton = document.querySelector('#login-section button');
 const loginForm = document.querySelector("#login-section")
+const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
 
-function onLoginSubmit(){
-  const value = loginInput.value;
-  console.log(value)
+const HIDDEN_CLASSNAME = "hidden"
+
+function onLoginSubmit(event){
+  event.preventDefault();
+  const name = loginInput.value;
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  // console.log(name);
+  greeting.innerText = `Hello ${name}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME)
+}
+
+function handleLinkClick(event) {
+  event.preventDefault();
+  alert("clicked");
+
 }
 loginForm.addEventListener("submit",onLoginSubmit)
+link.addEventListener("click",handleLinkClick)
