6fdf6c5ca80c5d39eec34e0ac0096cc0b8d78ed0
HyojaeKwon-GIST20 <khj010909@gm.gist.ac.kr>
2021-09-11 22:44:59 +0900

    save toDos

--- js-app/todo.js
+++ js-app/todo.js
@@ -2,12 +2,29 @@ const todoForm = document.getElementById("todo-form");
 const todoInput = document.querySelector("#todo-form input");
 const todoList = document.getElementById("todo-list");
 
+const toDos = [];
+
+function saveTodos(){
+  localStorage.setItem("todos", JSON.stringify(toDos));
+}
+
+function deleteTodo(event){
+  const li = event.target.parentElement;
+  li.remove();
+}
+
 function paintToDo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
+  const button = document.createElement("button");
+  button.innerText="X";
   span.innerText = newTodo;
+  toDos.push(newTodo);
   li.appendChild(span);
+  li.appendChild(button)
+  button.addEventListener("click",deleteTodo);
   todoList.appendChild(li);
+  saveTodos();
 }
 
 function handleToDoSubmit(event) {
